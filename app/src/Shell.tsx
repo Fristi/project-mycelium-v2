ab8e9aa046bc9148d4e06b6681663ae45f5522ec
Fristi <mark@vectos.net>
2025-07-31 12:43:36 +0200

    Work on the app

--- app/src/Shell.tsx
+++ app/src/Shell.tsx
@@ -22,14 +22,14 @@ export default function Shell() {
   return (
     <>
       <div className="min-h-full">
-        <Disclosure as="nav" className="bg-stone-600">
+        <Disclosure as="nav" className="bg-white">
           {({ open }) => (
             <>
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="flex h-16 items-center justify-between">
+                <div className="flex h-32 items-center justify-between">
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
-                      <img className="h-8 w-8" src="/logo.svg" alt="Mycelium" />
+                      <img className="h-32 w-32" src="/logo.jpeg" alt="Mycelium" />
                     </div>
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
